327cb4db4ed656f839321a49f48c3638c6096824
ModyQyW <261857860@qq.com>
2019-12-10 17:52:08 +0800

    Stack

---
+++ src/00.Stack/index.ts
@@ -0,0 +1,58 @@
+/**
+ * @description FILO
+ * @class Stack
+ */
+class Stack {
+  stack: any[] = []
+
+  constructor (...rest) {
+    if (rest.length > 0) {
+      throw new Error('Stack should initial with none params.')
+    }
+  }
+
+  /**
+   * @description Returns reference to the top element in the stack
+   * @memberof Stack
+   */
+  top (): any {
+    if (this.isEmpty()) {
+      return null
+    }
+    return this.stack[this.getSize() - 1]
+  }
+
+  /**
+   * @description Pushes the given element to the top of the stack
+   * @memberof Stack
+   */
+  push (ele): void {
+    this.stack.push(ele)
+  }
+
+  /**
+   * @description Removes the top element from the stack
+   * @memberof Stack
+   */
+  pop (): void {
+    this.stack.pop()
+  }
+
+  /**
+   * @description Returns the number of elements in the stack
+   * @memberof Stack
+   */
+  getSize (): number {
+    return this.stack.length
+  }
+
+  /**
+   * @description Checks if the stack has no elements
+   * @memberof Stack
+   */
+  isEmpty (): boolean {
+    return this.getSize() === 0
+  }
+}
+
+export default Stack
