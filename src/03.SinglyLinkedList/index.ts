e0380ecb9179d71e6980e8ea6b077c2cd3453650
ModyQyW <wurui7@mail3.sysu.edu.cn>
2020-01-13 23:41:58 +0800

    feat: use array to rewrite

--- src/03.SinglyLinkedList/index.ts
+++ src/03.SinglyLinkedList/index.ts
@@ -3,25 +3,10 @@
  * @class SinglyLinkedList
  */
 
-interface LinkedListNode {
-  val: any
-  next: ReturnedLinkedListNode
-}
+import { isFunction, isUndefined, isNull, isNumber } from 'lodash'
 
-type ReturnedLinkedListNode = LinkedListNode | null
-
-class SinglyLinkedListNode {
-  val: any = null
-  next: ReturnedLinkedListNode = null
-
-  constructor (val: any, next: ReturnedLinkedListNode) {
-    this.val = val
-    this.next = next
-  }
-}
-
-class SinglyLinkedList {
-  head: LinkedListNode = new SinglyLinkedListNode(null, null)
+class SinglyLinkedList<T> {
+  list: T[] = []
   size: number = 0
 
   constructor (...rest) {
@@ -30,27 +15,23 @@ class SinglyLinkedList {
     }
   }
 
-  addNode (val: any, index?: number | null): void {
-    if (Object.prototype.toString.call(index).slice(8, -1) !== 'Number' && index !== undefined && index !== null) {
-      throw new Error('Index should be a number, undefined or null.')
+  addNode (val: T, index?: number): void {
+    if (!isNumber(index) && !isUndefined(index)) {
+      throw new Error('Index should be a number or undefined.')
     }
-    const idx: number = index === undefined || index === null ? this.size : index
+    const idx: number = isUndefined(index) ? this.size : index
     if (idx > this.size || idx < 0) {
       if (this.size === 0) {
         throw new Error('Add node to the singly linked list failed cause out of boundary. You should add to the index 0.')
       }
       throw new Error(`Add node to the singly linked list failed cause out of boundary. You should add to the index between 0 and ${this.size}.`)
     }
-    let nodeTmp: ReturnedLinkedListNode = this.head
-    for (let i = 0; i < idx; i += 1) {
-      nodeTmp = nodeTmp.next as LinkedListNode
-    }
-    nodeTmp.next = new SinglyLinkedListNode(val, nodeTmp.next)
+    this.list.splice(idx, 0, val)
     this.size += 1
   }
 
   removeNode (index: number): void {
-    if (Object.prototype.toString.call(index).slice(8, -1) !== 'Number') {
+    if (!isNumber(index)) {
       throw new Error('Index should be a number.')
     }
     if (index > this.size || index < 0) {
@@ -59,11 +40,7 @@ class SinglyLinkedList {
       }
       throw new Error(`Remove node from the singly linked list failed cause out of boundary. You can remove from index between 0 and ${this.size}.`)
     }
-    let nodeTmp: ReturnedLinkedListNode = this.head
-    for (let i = 0; i < index; i += 1) {
-      nodeTmp = nodeTmp.next as LinkedListNode
-    }
-    nodeTmp.next = nodeTmp.next === null ? null : nodeTmp.next.next
+    this.list.splice(index, 1)
     this.size -= 1
   }
 
@@ -71,40 +48,36 @@ class SinglyLinkedList {
    * @description Returns reference to the first node in the singly linked list
    * @memberof SinglyLinkedList
    */
-  getFirst (): ReturnedLinkedListNode {
-    return this.head.next === null ? null : this.head.next.val
+  getFirst (): T | null {
+    if (this.size === 0) {
+      return null
+    }
+    return this.list[0]
   }
 
   /**
    * @description Returns reference to the last node in the singly linked list
    * @memberof SinglyLinkedList
    */
-  getLast (): ReturnedLinkedListNode {
-    let nodeTmp: ReturnedLinkedListNode = this.head.next as LinkedListNode
-    if (nodeTmp === null) {
+  getLast (): T | null {
+    if (this.size === 0) {
       return null
     }
-    while (nodeTmp.next !== null) {
-      nodeTmp = nodeTmp.next
-    }
-    return nodeTmp.val
+    return this.list[this.size - 1]
   }
 
-  getNode (index: number): ReturnedLinkedListNode {
-    if (Object.prototype.toString.call(index).slice(8, -1) !== 'Number') {
+  getNode (index: number): T | null {
+    if (!isNumber(index)) {
       throw new Error('Index should be a number.')
     }
     if (index > this.size || index < 0) {
       if (this.size === 0) {
-        throw new Error('Get node from the singly linked list failed cause out of boundary. You can only get from index 0.')
+        throw new Error('Get node from the singly linked list failed cause out of boundary.  You can only get index 0.')
       }
       throw new Error(`Get node from the singly linked list failed cause out of boundary. You can get from index between 0 and ${this.size}.`)
     }
-    let nodeTmp: ReturnedLinkedListNode = this.head
-    for (let i = 0; i < index; i += 1) {
-      nodeTmp = nodeTmp.next as LinkedListNode
-    }
-    return nodeTmp.next === null ? null : nodeTmp.next.val
+    const node = this.list[index]
+    return isUndefined(node) ? null : node
   }
 
   /**
@@ -126,28 +99,37 @@ class SinglyLinkedList {
 
 const singlyLinkedList = new SinglyLinkedList()
 
-console.log('first node', singlyLinkedList.getFirst())
-console.log('last node', singlyLinkedList.getLast())
-console.log('getNode0', singlyLinkedList.getNode(0))
-// console.log('getNode1', singlyLinkedList.getNode(1)) // error
-console.log('size', singlyLinkedList.getSize())
-console.log('isEmpty', singlyLinkedList.isEmpty())
-console.log('\r\n')
+console.log('getFirst()', singlyLinkedList.getFirst()) // null
+console.log('getLast', singlyLinkedList.getLast()) // null
+console.log('getNode(0)', singlyLinkedList.getNode(0)) // null
+// console.log('getNode(1)', singlyLinkedList.getNode(1)) // error
+console.log('getSize()', singlyLinkedList.getSize()) // 0
+console.log('isEmpty()', singlyLinkedList.isEmpty()) // true
+console.log('\r')
 
-console.log('addNode(1)', singlyLinkedList.addNode(1))
-console.log('addNode(0, 0)', singlyLinkedList.addNode(0, 0))
-console.log('first node', singlyLinkedList.getFirst())
-console.log('last node', singlyLinkedList.getLast())
-console.log('getNode0', singlyLinkedList.getNode(0))
-console.log('getNode1', singlyLinkedList.getNode(1))
-console.log('size', singlyLinkedList.getSize())
-console.log('isEmpty', singlyLinkedList.isEmpty())
-console.log('\r\n')
+console.log('addNode(1)', singlyLinkedList.addNode(1)) // undefined
+console.log('addNode(0, 0)', singlyLinkedList.addNode(0, 0)) // undefined
+console.log('getFirst()', singlyLinkedList.getFirst()) // 0
+console.log('getLast()', singlyLinkedList.getLast()) // 1
+console.log('getNode(0)', singlyLinkedList.getNode(0)) // 0
+console.log('getNode(1)', singlyLinkedList.getNode(1)) // 1
+console.log('getSize()', singlyLinkedList.getSize()) // 2
+console.log('isEmpty()', singlyLinkedList.isEmpty()) // false
+console.log('\r')
 
-console.log('removeNode(0)', singlyLinkedList.removeNode(0))
-console.log('first node', singlyLinkedList.getFirst())
-console.log('last node', singlyLinkedList.getLast())
-console.log('getNode0', singlyLinkedList.getNode(0))
-// console.log('getNode1', singlyLinkedList.getNode(1)) // error
-console.log('size', singlyLinkedList.getSize())
-console.log('isEmpty', singlyLinkedList.isEmpty())
+console.log('removeNode(1)', singlyLinkedList.removeNode(1)) // undefined
+console.log('getFirst()', singlyLinkedList.getFirst()) // 0
+console.log('getLast()', singlyLinkedList.getLast()) // 0
+console.log('getNode(0)', singlyLinkedList.getNode(0)) // 0
+// console.log('getNode(1)', singlyLinkedList.getNode(1)) // error
+console.log('getSize()', singlyLinkedList.getSize()) // 1
+console.log('isEmpty()', singlyLinkedList.isEmpty()) // false
+console.log('\r')
+
+console.log('removeNode(0)', singlyLinkedList.removeNode(0)) // undefined
+console.log('getFirst()', singlyLinkedList.getFirst()) // null
+console.log('getLast()', singlyLinkedList.getLast()) // null
+console.log('getNode(0)', singlyLinkedList.getNode(0)) // null
+// console.log('getNode(1)', singlyLinkedList.getNode(0)) // error
+console.log('getSize()', singlyLinkedList.getSize()) // 0
+console.log('isEmpty()', singlyLinkedList.isEmpty()) // true
