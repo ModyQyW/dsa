c2bb7868b013218f416f0c5e553256633995fb55
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-02-23 13:05:27 +0800

    fix: fix wrong configs in rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,56 +1,53 @@
 import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
 import dts from 'rollup-plugin-dts';
-import fs from 'fs';
 import path from 'path';
 
 const configs = [];
 
-fs.readdirSync(path.resolve('src')).forEach((dir) => {
-  configs.push(
-    {
-      input: path.resolve('src', 'index.ts'),
-      output: [
-        {
-          file: path.resolve('dist', 'index.cjs.js'),
-          format: 'cjs',
-        },
-        {
-          file: path.resolve('dist', 'index.esm.js'),
-          format: 'es',
-        },
-        {
-          file: path.resolve('dist', 'index.iife.js'),
-          format: 'iife',
-          name: 'dsa',
-          extend: true,
-        },
-        {
-          file: path.resolve('dist', 'index.iife.min.js'),
-          format: 'iife',
-          name: 'dsa',
-          extend: true,
-          plugins: [
-            terser({
-              format: {
-                comments: false,
-              },
-            }),
-          ],
-        },
-      ],
-      plugins: [typescript()],
-    },
-    {
-      input: path.resolve('src', 'index.ts'),
-      output: {
-        file: path.resolve('dist', 'index.d.ts'),
+configs.push(
+  {
+    input: path.resolve('src', 'index.ts'),
+    output: [
+      {
+        file: path.resolve('dist', 'index.cjs.js'),
+        format: 'cjs',
+      },
+      {
+        file: path.resolve('dist', 'index.esm.js'),
         format: 'es',
       },
-      plugins: [dts()],
+      {
+        file: path.resolve('dist', 'index.iife.js'),
+        format: 'iife',
+        name: 'dsa',
+        extend: true,
+      },
+      {
+        file: path.resolve('dist', 'index.iife.min.js'),
+        format: 'iife',
+        name: 'dsa',
+        extend: true,
+        plugins: [
+          terser({
+            format: {
+              comments: false,
+            },
+          }),
+        ],
+      },
+    ],
+    plugins: [typescript()],
+  },
+  {
+    input: path.resolve('src', 'index.ts'),
+    output: {
+      file: path.resolve('dist', 'index.d.ts'),
+      format: 'es',
     },
-  );
-});
+    plugins: [dts()],
+  },
+);
 
 export default configs;
 
