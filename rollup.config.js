ec62f515b8ade0ff47259cded5bea4db24553714
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-02-23 14:08:40 +0800

    build: replace @rollup/plugin-typescript with rollup-plugin-typescript2

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import typescript from '@rollup/plugin-typescript';
+import typescript from 'rollup-plugin-typescript2';
 import { terser } from 'rollup-plugin-terser';
 import dts from 'rollup-plugin-dts';
 import path from 'path';
