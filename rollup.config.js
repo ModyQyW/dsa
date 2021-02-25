50f70dd9c2b4c9fc62483cec0d246c10061172bc
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-02-26 00:38:01 +0800

    build: build and use min files by default, name dsa -> DSA

--- rollup.config.js
+++ rollup.config.js
@@ -7,38 +7,53 @@ import path from 'path';
 const configs = [];
 
 configs.push(
+  // {
+  //   input: path.resolve('src', 'index.ts'),
+  //   output: [
+  //     {
+  //       file: path.resolve('dist', 'index.cjs.js'),
+  //       format: 'cjs',
+  //     },
+  //     {
+  //       file: path.resolve('dist', 'index.esm.js'),
+  //       format: 'es',
+  //     },
+  //     {
+  //       file: path.resolve('dist', 'index.iife.js'),
+  //       format: 'iife',
+  //       name: 'DSA',
+  //       extend: true,
+  //     },
+  //   ],
+  //   plugins: [nodeResolve(), typescript()],
+  // },
   {
     input: path.resolve('src', 'index.ts'),
     output: [
       {
-        file: path.resolve('dist', 'index.cjs.js'),
+        file: path.resolve('dist', 'index.cjs.min.js'),
         format: 'cjs',
       },
       {
-        file: path.resolve('dist', 'index.esm.js'),
+        file: path.resolve('dist', 'index.esm.min.js'),
         format: 'es',
-      },
-      {
-        file: path.resolve('dist', 'index.iife.js'),
-        format: 'iife',
-        name: 'dsa',
-        extend: true,
       },
       {
         file: path.resolve('dist', 'index.iife.min.js'),
         format: 'iife',
-        name: 'dsa',
+        name: 'DSA',
         extend: true,
-        plugins: [
-          terser({
-            format: {
-              comments: false,
-            },
-          }),
-        ],
       },
     ],
-    plugins: [nodeResolve(), typescript()],
+    plugins: [
+      terser({
+        format: {
+          comments: false,
+        },
+      }),
+      nodeResolve(),
+      typescript(),
+    ],
   },
   {
     input: path.resolve('src', 'index.ts'),
