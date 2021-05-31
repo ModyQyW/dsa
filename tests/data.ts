c86ea1e8bf908cf7884b10c50cad5a32ad699383
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-05-31 08:36:26 +0800

    feat: add compareFn in tests

---
+++ tests/data.ts
@@ -0,0 +1,9 @@
+export const compareNumbers1 = (a: number, b: number) => a - b;
+// if (a < b) return -1;
+// if (a === b) return 0;
+// return 1;
+
+export const compareNumbers2 = (a: number, b: number) => b - a;
+// if (a < b) return 1;
+// if (a === b) return 0;
+// return -1;
