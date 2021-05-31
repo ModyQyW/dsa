d46018e0a2d99ed02d3695a04ac10f8015ecbaca
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-05-31 08:26:24 +0800

    perf: bubble-sort

--- src/bubble-sort/index.ts
+++ src/bubble-sort/index.ts
@@ -1,16 +1,13 @@
 /* eslint-disable import/prefer-default-export */
 import { isArray, isFunction, cloneDeep } from 'lodash-es';
 
-export function bubbleSort<T>(
-  array: T[],
-  compareFn: (a: T, b: T) => -1 | 0 | 1,
-) {
+export function bubbleSort<T>(array: T[], compareFn: (a: T, b: T) => number) {
   if (!isArray(array)) {
     throw new Error('You should pass a array as the first param.');
   }
   if (!isFunction(compareFn)) {
     throw new Error(
-      'You should pass a function as the second param, which accepts two params and returns -1, 0 or 1.',
+      'You should pass a function as the second param, which accepts two params and returns a number.',
     );
   }
   const newArray = cloneDeep(array);
@@ -18,8 +15,8 @@ export function bubbleSort<T>(
   for (let i = 0, { length } = newArray; i < length; i += 1) {
     let isSwapped = false;
     // inner loop, move the smaller/larger elements to the end
-    for (let j = 0; j < length - 1 - i; j += 1) {
-      if (compareFn(newArray[j], newArray[j + 1]) === 1) {
+    for (let j = 0, boundary = length - 1 - i; j < boundary; j += 1) {
+      if (compareFn(newArray[j], newArray[j + 1]) > 0) {
         isSwapped = true;
         [newArray[j], newArray[j + 1]] = [newArray[j + 1], newArray[j]];
       }
